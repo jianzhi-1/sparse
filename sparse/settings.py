32e606ffce0acdb0cc26c0c23ce184e57f7d46fa
jianzhi-1 <jianzhiw00@gmail.com>
2020-05-09 16:06:52 +0800

    added files from tutorial

--- sparse/settings.py
+++ sparse/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'sparser.apps.SparserConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
